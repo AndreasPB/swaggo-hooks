681f98ed95e755f429156a8f8f425dd2d954a960
AndreasPB <andreas.p.brodersen@gmail.com>
2022-12-05 11:10:26 +0100

    testing stuff - trying to install swag during CI

--- run-swag-fmt.sh
+++ run-swag-fmt.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+go install github.com/swaggo/swag/cmd/swag@latest
+
 if ! which swag &> /dev/null
 then
+    echo "yolo"
     echo "swag could not be found. Please ensure it is installed and available on your PATH."
-    exit 1
+    # exit 1
 fi
 
 swag fmt "$@"
